abca1e454773198f5cec5512c700e32152ab914d
wilson3849 <wilson3849@gmail.com>
2022-03-17 21:23:36 +0000

    Add README.md and refactor the unwanted message

--- roverDrive.ts
+++ roverDrive.ts
@@ -7,32 +7,24 @@ export class roverDrive {
     constructor(){}
     
     move(map:navMap, position:navPos , move:moveType) {
-        var headDirection = "NWSE"
+        const headDirection = "NWSE"
 
-        var moveToPos = position 
-        //console.log("Start from (" + moveToPos.x.toString() + "," + moveToPos.y.toString()+ ")")
+        let moveToPos = position 
         switch(move){
             case 'L':
-                //console.log("From " + position.head)
                 moveToPos.head = (headDirection.indexOf(moveToPos.head) < headDirection.length - 1) ? headDirection[headDirection.indexOf(moveToPos.head) + 1] as direction : headDirection[0] as direction
-                //console.log("turn left to " + moveToPos.head)
                 break
             case 'R':
-                //console.log("From " + position.head)
                 moveToPos.head = (headDirection.indexOf(moveToPos.head) !== 0) ? headDirection[headDirection.indexOf(moveToPos.head) - 1] as direction : headDirection[headDirection.length - 1] as direction
-                //console.log("turn right to " + moveToPos.head)
                 break
             case 'M':
-                //console.log("move ahead.")
                 if (moveToPos.head === 'N'){ moveToPos.y < map.length ? moveToPos.y += 1 : moveToPos }
                 if (moveToPos.head === 'W'){ moveToPos.x > 0 ? moveToPos.x -= 1 : moveToPos }
                 if (moveToPos.head === 'S'){ moveToPos.y > 0 ? moveToPos.y -= 1 : moveToPos }
                 if (moveToPos.head === 'E'){ moveToPos.x < map.width ? moveToPos.x += 1 : moveToPos }
-                //console.log("rover head to " + moveToPos.head + ",move to (" + moveToPos.x.toString() + "," + moveToPos.y.toString()+ ") in (" + map.length.toString() + "," + map.width.toString() + ")")
                 break
             default: 
                 break     
         }
     }
-
 }
